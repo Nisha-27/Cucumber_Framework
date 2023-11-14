2b2359b43cdd53b27ae9d86ea5e5f985d4e29586
Nisha-27 <nisharavi2712@gmail.com>
2023-11-14 16:39:09 +0530

    test

--- src/test/java/step_Definition/NewClassTry.java
+++ src/test/java/step_Definition/NewClassTry.java
@@ -1,9 +1,12 @@
+
 package step_Definition;
 
-public class Conflict {
+public class NewClassTry {
+
 	
 	public static void main(String[] args) {
-		System.out.println("added from shalini");
+		
+		System.out.println("local");
+		
 	}
-
 }
